573d11062be65490a2cf757d3650566317a43feb
mjpoland22 <124516766+mjpoland22@users.noreply.github.com>
2025-01-23 18:35:36 -0800

    Update backend.tf

--- 2-terraform-eks-deployment/backend.tf
+++ 2-terraform-eks-deployment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-kub-2024-v2"
-    region = "eu-west-3"
+    bucket = "jenkins-app-kub-2024-v2-mjp222"
+    region = "us-east-2"
     key = "eks/terraform.tfstate"
   }
 }
