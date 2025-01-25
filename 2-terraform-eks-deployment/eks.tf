141453376794f8466baaf69aa4aaf9b23ec32924
MJP222 <124516766+mjpoland22@users.noreply.github.com>
2025-01-24 18:47:07 -0800

    Update eks.tf

--- 2-terraform-eks-deployment/eks.tf
+++ 2-terraform-eks-deployment/eks.tf
@@ -1,8 +1,8 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
-    cluster_name = "my-eks-cluster"
-    cluster_version = "1.24"
+    cluster_name = "wlab-eks-cluster"
+    cluster_version = "1.31"
 
     cluster_endpoint_public_access  = true
 
